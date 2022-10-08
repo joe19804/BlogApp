f9f1d098293c7d8d9b001d7d62ed17bfebdf1586
yeliwei <a0970366492@gmail.com>
2022-10-08 23:32:16 +0800

    add register login logout function

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,31 +1,68 @@
 import { db } from '../db.js';
-import crypto from 'crypto';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-export const register = async (req, res) => {
-    const findUser = "SELECT * FROM users WHERE username = ? OR email = ?";
+export const register = async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    if (!username || username.length < 3) {
+        return res.status(400).json('Must be at least 3 characters or more')
+    }
+    if (!email || !email.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+        return res.status(400).json('Your email message should be formatted like a typical business letter')
+    }
+    if (!password || password.length < 8 || !password.match(/^(?=.*[0-9])((?=.*[a-z])|(?=.*[A-Z])).*$/)) {
+        return res.status(400).json('Must be at least 8 characters or more, needs at least one number and one letter')
+    }
+
     try {
-        const [rows, fields] = await db.promise().query(findUser, [req.body.email, req.body.username]);
+        const findUser = 'SELECT * FROM users WHERE username = ? OR email = ?';
+        const [rows, fields] = await db.promise().query(findUser, [username, email]);
 
         if (rows.length) {
-            return res.status(409).json("User already exists!");
+            return res.status(409).json('User already exists!');
         }
 
-        const salt = crypto.randomBytes(16).toString('hex');
-        const hash = crypto.pbkdf2Sync(req.body.password, salt, 1000, 64, `sha512`).toString(`hex`);
-        const createUser = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        const createUser = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+        await db.promise().query(createUser, [username, email, hash]);
 
-        await db.promise().query(createUser, [req.body.username, req.body.email, hash]);
-        db.release();
-        return res.status(200).json("User has been created.");
+        return res.status(200).json('User has been created.');
     } catch (error) {
-        return res.status(500).json(error);
+        next(error);
     }
 }
 
-export const login = (req, res) => {
-    res.json("login")
+export const login = async (req, res, next) => {
+    try {
+        const findUser = 'SELECT * FROM users WHERE username = ?';
+        const [rows, fields] = await db.promise().query(findUser, [req.body.username]);
+
+        if (rows.length === 0) {
+            return res.status(404).json('User not found!');
+        }
+        const { password, ...userInfo } = rows[0];
+        const validPassword = bcrypt.compareSync(req.body.password, password);
+
+        if (!validPassword) {
+            return res.status(400).json('Wrong username or password!');
+        }
+
+        const token = jwt.sign({ id: rows[0].id }, 'jtwsecret');
+
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json(userInfo);
+
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const logout = (req, res) => {
-    res.json("logout")
+    res.clearCookie("access_token", {
+        sameSite: "none",
+        secure: true
+    }).status(200).json('User has been logged out.')
 }
--- api/index.js
+++ api/index.js
@@ -1,17 +1,30 @@
 import express from "express";
-import postRouter from './routes/posts.js'
-import authRouter from './routes/auth.js'
-import usersRouter from './routes/users.js'
+import postRouter from './routes/posts.js';
+import authRouter from './routes/auth.js';
+import usersRouter from './routes/users.js';
+import cookieParser from "cookie-parser";
 
 const app = express();
-
 const port = 5000;
 
+const errorResponder = (err, req, res, next) => {
+    res.header('Content-Type', 'application/json')
+    const status = err.status || 500;
+    res.status(status).json({ error: err.message });
+}
+
+const invalidPathHandler = (req, res) => {
+    res.status(404).json({ error: 'invalid path' });
+}
+
 app.use(express.json());
-app.use(express.urlencoded());
-app.use("/api/posts", postRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/users", usersRouter);
+app.use(cookieParser());
+app.use('/api/posts', postRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use(errorResponder);
+app.use(invalidPathHandler);
+
 
 
 app.listen(port, () => {
