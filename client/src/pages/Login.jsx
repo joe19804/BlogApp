82d3ef8a3240908e5900dc30e3325e7902ab9d59
Cathy <cathy19804@gmail.com>
2022-10-08 21:08:06 +0800

    1008 2100更

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
   return (
     <div className='auth'>
         <h1>Login</h1>
         <form>
-          <input type="text" placeholder='username' />
-          <input type="password" placeholder='password' />
+          <input required type="text" placeholder='username' />
+          <input required type="password" placeholder='password' />
           <button>Login</button>
+          <p>忘記密碼</p>
+          <span>尚未有帳號?<Link to="/register">Register</Link>
+          </span>
         </form>
     </div>
   )
