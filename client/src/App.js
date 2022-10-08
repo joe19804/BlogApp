9a64d41d9d27932b0775995403742236ac53eb40
Cathy <cathy19804@gmail.com>
2022-10-08 18:32:01 +0800

    1008 1830更新

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,59 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Home from "./pages/Home"
+import Single from "./pages/Single"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = ()=>{
+  return (
+    <>
+      <Navbar/>
+      <Ouylet/>
+      <Footer/>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/single",
+        element:<Single/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router}/>
     </div>
   );
 }
